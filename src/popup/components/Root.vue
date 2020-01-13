cde1146af781fb27d269e753b9a495a509c88ce8
Naoto Kaneko <naoty.k@gmail.com>
2020-01-13 23:58:17 +0900

    Apply styles inside popup

--- src/popup/components/Root.vue
+++ src/popup/components/Root.vue
@@ -1,6 +1,15 @@
 <template>
-  <h1>Here is Root.vue.</h1>
+  <div class="container">
+    <h1>esa-history</h1>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  width: 300px;
+  height: 300px;
+}
+</style>
 
 <script>
 export default {};
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
       }
     ]
   },
