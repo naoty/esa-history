243b140cfe916663497df12f00f9300f81424ab0
Naoto Kaneko <naoty.k@gmail.com>
2020-01-13 12:10:08 +0900

    Add background script

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  mode: "development",
+  entry: {
+    background: ["./src/background/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]/bundle.js"
+  },
   plugins: [
     new CopyPlugin([
       { from: "src/manifest.json", to: "manifest.json" },
