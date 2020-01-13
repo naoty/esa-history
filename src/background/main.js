b44521f9ae793dbefb37aa57962bcf74a8c3894c
Naoto Kaneko <naoty.k@gmail.com>
2020-01-13 12:25:52 +0900

    Allow extension only when *.esa.io is opened.

--- src/background/main.js
+++ src/background/main.js
@@ -1,1 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {});
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+              hostContains: ".esa.io"
+            }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
