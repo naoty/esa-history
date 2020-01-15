7693549307ed09cc4366aa3028becfb40146f15a
Naoto Kaneko <naoty.k@gmail.com>
2020-01-15 22:24:15 +0900

    Add temporary markup and styles

--- src/popup/components/Root.vue
+++ src/popup/components/Root.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="container">
-    <h1>esa-history</h1>
+    <ul>
+      <li>
+        <a href="#">Naoto Kaneko</a>
+      </li>
+      <li>
+        <a href="#">pt-archiver</a>
+      </li>
+      <li>
+        <a href="#">#1222: 2年以上使われていない集計用テーブルの削除</a>
+      </li>
+      <li>
+        <a href="#">#1197: member環境の管理にfor_eachを使えるか</a>
+      </li>
+      <li>
+        <a href="#">PushのIdentification情報</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
 .container {
-  width: 300px;
-  height: 300px;
+  font-size: 16px;
+  min-width: 20em;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  border-bottom: 1px solid #aaa;
+}
+
+a {
+  display: block;
+  padding: 0.25em;
+  text-decoration: none;
 }
 </style>
 
