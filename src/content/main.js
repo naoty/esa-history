0f355d0cea79f202fe1fc335539dc6d7d2c78fa1
Naoto Kaneko <naoty.k@gmail.com>
2020-01-16 22:36:45 +0900

    Save browsing histories in sync storage

--- src/content/main.js
+++ src/content/main.js
@@ -1,1 +1,9 @@
-console.log("Here is content script");
+const url = location.href;
+const id = parseInt(url.split("/").pop());
+const title = document.title.split(" - ")[0];
+const timestamp = Date.now();
+
+const post = {};
+post[id] = { id, url, title, timestamp };
+
+chrome.storage.sync.set(post);
