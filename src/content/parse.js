28fb9a75ce1777dd78f51d99a9407fcedac6f606
Naoto Kaneko <naoty.k@gmail.com>
2020-02-01 18:46:58 +0900

    Save the author of visited post
    It maybe be used as search keyword.

--- src/content/parse.js
+++ src/content/parse.js
@@ -3,6 +3,7 @@ export default function() {
   const id = parseInt(url.split("/").pop());
   const title = document.title.split(" - ")[0];
   const timestamp = Date.now();
+  const author = document.querySelector(".post-author__user > a").innerText;
 
-  return { id, url, title, timestamp };
+  return { id, url, title, timestamp, author };
 }
