490368fd4498b8b1bbacefc551d3a748feda973f
Naoto Kaneko <naoty.k@gmail.com>
2020-02-01 18:47:45 +0900

    Stop removing less recently visited posts up to 5

--- src/content/main.js
+++ src/content/main.js
@@ -7,12 +7,4 @@ if (location.href.match(/esa\.io\/posts\/\d+$/) !== null) {
   item[post.id] = post;
 
   chrome.storage.sync.set(item);
-
-  chrome.storage.sync.get(null, items => {
-    const posts = Object.values(items);
-    posts.sort((a, b) => (a.timestamp - b.timestamp) * -1);
-
-    const ids = posts.slice(5).map(post => post.id.toString());
-    chrome.storage.sync.remove(ids);
-  });
 }
